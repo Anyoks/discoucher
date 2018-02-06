53f87b4f8077aa7f543b1f8820fcad17f1af09d2
Denn Orina <dennorina@gmail.com>
2018-02-06 21:08:20 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+  
+  
+
+  devise_for :users
+  devise_scope :user do
+      get 'sign_in', to: 'devise/sessions#new'
+      get 'sign_up', to: 'devise/registrations#new'
+  end
+
+  authenticated :user do
+    root to: 'book#index', as: :authenticated_root
+  end
+
+
   resources :establishments
   resources :vouchers
   resources :books
-  root 'books#index'
+  # root 'establishments#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
