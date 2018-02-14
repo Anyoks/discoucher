21c42e6b451fe7bd72e6a7280014f7045e9a22ad
Denn Orina <dennorina@gmail.com>
2018-02-14 07:49:47 +0000

    added afew comments

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -15,6 +15,7 @@
 class Voucher < ApplicationRecord
 	belongs_to :establishment
 
+	# Check if the voucher is redeemed or not and return true or false
 	def redeemed?
 		if self.redeem_status == true
 			"Voucher redeemed!!"
@@ -26,26 +27,37 @@ class Voucher < ApplicationRecord
 		
 	end
 
+	# Manually redeen a voucher if you have a code.
 	def manually_redeem  voucher_code
+		# find the voucher
 		voucher = Voucher.where(code: voucher_code).first
 
+		# Does that voucher actually exist and is it redeemed already? if yes and no, 
+		# redeem it
 		if voucher.present? && voucher.redeem_status == false
-			voucher.redeem
-			"successfully redeemed"
-			# return true
+
+			# redeem it.
+			if voucher.redeem
+				"successfully redeemed"
+				return true
+			else
+				return false
+			end
 		else
 			"The voucher does not exist or has already been used"
-			# return false
+			return false
 		end
 	end
 
+	# Voucher redemption process
 	def redeem
-		# first check if the book is registered
+		# first check if the book is registered if so start voucher redemption process
 		if self.check_if_book_is_registered
-			# check if it's aready redeemed
+			
+			# check if it's aready redeemed if not redeem it
 			
 			if self.redeem_status == false
-			# change redeemed status
+			# change redeemed status to make the voucher redeemed
 				if self.update_attributes(redeem_status: true)
 					"The voucher has been successfully redeemed"
 					return true
@@ -63,6 +75,7 @@ class Voucher < ApplicationRecord
 		end
 	end
 
+	# attempt manual redemption from an already existing sms
 	def redeem_from_sms sms_object
 		
 	end
@@ -72,7 +85,7 @@ class Voucher < ApplicationRecord
 	def check_if_book_is_registered
 		if self.establishment.book.registered?.present?
 			"This Book is registered"
-			# return true
+			return true
 		else
 			return false
 		end		
