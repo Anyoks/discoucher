f2bae8fd860083541c06206e48e9a152b556f7a4
Denn Orina <anyoksdenn@gmail.com>
2018-02-23 13:58:56 +0000

    syntax error hot fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -72,6 +72,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:code, :year
+      params.require(:book).permit(:code, :year)
     end
 end
