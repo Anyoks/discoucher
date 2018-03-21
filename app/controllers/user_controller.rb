3538b26887ec8b0ceae1acbb1f2c79a881e7a822
Denn Orina <anyoksdenn@gmail.com>
2018-03-21 08:18:45 +0000

    some ui additions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,18 @@
 class UserController < ApplicationController
   def index
   	@users = User.all.order(created_at: :desc)
+
+    # Bar chart
+    @vists_data = overal_visits_by_month
+    @options1 = {:height => "257px", :width => "514px"}
+
+    # doughnut
+    @data = establisments_visits
+    @options = {:height => "257px", :width => "514px"}
+
+    # least and most active users
+    @most_active = most_active
+    @least_active = least_active
   end
 
   def show
@@ -108,6 +120,85 @@ class UserController < ApplicationController
     
     
   end
+
+  def overal_visits_by_month
+      base = Visit.group_by_month(:created_at).count
+
+      months = base.map {|label, data| label.strftime("%B")}
+      establishment_vists_count = base.map {|label, data| data}
+
+      colour = []
+      establishment_vists_count.size.times { |c| colour <<"#%06x" % (rand * 0xffffff) }
+
+      @visits_by_month = {
+        labels: months,
+        datasets: [
+          {
+             # label: "My First dataset",
+             # pointStrokeColor: "#FFA500",
+             # fillColor: "rgba(255,165,0,0.5)",
+             # pointColor: "rgba(255,165,0,1)",
+             # strokeColor: "rgba(255,165,0,1)",
+             data: establishment_vists_count,#array_of_data_four,
+             backgroundColor: colour,
+             hoverBackgroundColor: "#FFA500"
+
+          }
+        ]
+      }
+  end
+
+  def establisments_visits
+      base = User.select(:first_name).joins(:visits).select(:first_name)
+      establishment_names = base.to_a.group_by(&:first_name).map {|label, data| label}
+      establishment_vists_count = base.to_a.group_by(&:first_name).map {|label, data| data.count}
+
+      colour = []
+      establishment_vists_count.size.times { |c| colour <<"#%06x" % (rand * 0xffffff) }
+
+      @visits_by_month = {
+        labels: establishment_names,
+        datasets: [
+          {
+             # label: "My First dataset",
+             # pointStrokeColor: "#FFA500",
+             # fillColor: "rgba(255,165,0,0.5)",
+             # pointColor: "rgba(255,165,0,1)",
+             # strokeColor: "rgba(255,165,0,1)",
+             data: establishment_vists_count,#array_of_data_four,
+             backgroundColor: colour,
+             hoverBackgroundColor: "#FFA500"
+
+          }
+        ]
+      }
+      
+  end
+
+  def most_active
+      most_and_least_active = get_most_least
+      most_active_name = most_and_least_active[1].first.user.first_name   
+      most_active_count = most_and_least_active[1].size
+      array = []
+
+      array << most_active_name << most_active_count
+      return array      
+    end
+
+    def least_active
+      least_active_name = get_most_least
+      least_active_name = least_active_name[0].first.user.first_name
+      least_active_count = least_active_name[0].size
+      array = []
+
+      array << least_active_name << least_active_count
+      return array
+    end
+
+    def get_most_least
+      most_and_least_active = Visit.select(:user_id).group_by(&:user_id).map {|name, data| data}.minmax
+      return most_and_least_active
+    end
 
   def get_names array
     names = []
