a5a699483e1c981ef8042e88190785483613d7a6
Denn Orina <anyoksdenn@gmail.com>
2018-07-24 17:22:17 +0000

    changed image resolutions

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,7 +4,7 @@ class Picture < ApplicationRecord
   has_attached_file :image,
      :path => ":rails_root/public/images/:id/:style_:filename",
      :url  => "/images/:id/:style_:filename",
-     :styles => { small: "64x64", medium: "100x100", large: "200x200" }
+     :styles => { thumb: "200x200", small: "600x600", medium: "800x800", large: "1000x1000" }
 
    do_not_validate_attachment_file_type :image
 
