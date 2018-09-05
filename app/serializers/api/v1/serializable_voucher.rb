dc7073ea3b2d31c3d3d7e8d8c66c26264e90e6a8
Denn Orina <anyoksdenn@gmail.com>
2018-09-05 13:03:06 +0000

    added establishment details

--- app/serializers/api/v1/serializable_voucher.rb
+++ app/serializers/api/v1/serializable_voucher.rb
@@ -2,12 +2,16 @@ class  Api::V1::SerializableVoucher < JSONAPI::Serializable::Resource
   type :vouchers
 
   attribute :code
-  attribute :description
-  attribute :condition
   attribute :establishment_id
   attribute :establishment_name do
-  	@object.establishment.name
+    @object.establishment.name
   end
+  attribute :establishment_type do
+    @object.establishment.establishment_type.name
+  end
+  attribute :description
+  attribute :condition
+  
   attribute :est_image do
   	 @object.establishment.featured_image.present? ? @object.establishment.featured_image : ActionController::Base.helpers.asset_path('discoucher_small.jpg')
   end
