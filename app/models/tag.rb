b818026b3ee04407d7fd328df2ab27d19ee54094
Denn Orina <anyoksdenn@gmail.com>
2018-09-14 12:48:27 +0000

    added featured image for tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,26 @@ class Tag < ApplicationRecord
 
 	has_many :tagpics
 	has_and_belongs_to_many :vouchers
+
+
+	def featured_image
+
+		if Rails.env == "development" 
+			root_url = "http://localhost:3000"
+		else
+			root_url = "http://159.89.103.53"
+		end
+
+
+		tagpic = self.tagpics
+
+		if tagpic.present?
+			image = root_url + tagpic.first.image.url(:medium)
+		else
+			image = nil
+		end
+		
+		# image = root_url + tagpic
+		return image
+	end
 end
