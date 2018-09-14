e8d72a89ad84b16b0e8d688ec1e705aa2eb7f4e6
Denn Orina <anyoksdenn@gmail.com>
2018-09-14 12:50:19 +0000

    changed default image url

--- app/models/tagpic.rb
+++ app/models/tagpic.rb
@@ -4,7 +4,7 @@ class Tagpic < ApplicationRecord
 					  :path => ':rails_root/public/system/:class/:attachment/:id/:style_:filename',
 					  :url => "/system/:class/:attachment/:id/:style_:filename",
 					  :styles => { small: "64x64", medium: "100x100", large: "200x200" },
-					  :default_url => "/assets/missing.png"
+					  :default_url => "missing.png"
 
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
