72653d877dbfa10d018d1233f2d176380bca4614
Denn Orina <anyoksdenn@gmail.com>
2018-10-10 17:07:57 +0000

    create reg book fo rpaying user

--- app/models/payment_response.rb
+++ app/models/payment_response.rb
@@ -1,4 +1,38 @@
 class PaymentResponse < ApplicationRecord
 
 	belongs_to :payment_request
+	after_commit :create_registered_book_for_payed_user, on: :create
+	# after_commit :add_book_code, on: :update
+
+
+
+	def user
+		self.payment_request.user
+	end
+
+
+	private
+
+	def create_registered_book_for_payed_user
+		user = self.user
+
+		if self.Amount.to_f > 0
+			reg 	= user.register_book_for_paying_mobile_user
+
+			if reg == false
+				logger.debug "Error creating registered Book for user"
+				return false
+			else
+				t = self.update_columns(book_code: reg.book_code)
+				return true
+			end
+		else
+			logger.debug "Not enough money"
+			return false
+		end		
+	end
+
+	def add_book_code
+		self.update_attributes(book_code: reg.book_code)
+	end
 end
