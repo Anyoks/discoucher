d4498691ae55f6a6d21ead0f309bdde900d4e486
Denn Orina <anyoksdenn@gmail.com>
2018-10-30 12:13:43 +0000

    added dupe detection

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,7 @@ class Tag < ApplicationRecord
 
 	has_many :tagpics
 	has_and_belongs_to_many :vouchers
+	before_save { |tag| tag.name = tag.name.downcase }
 
 
 	def featured_image
@@ -24,5 +25,40 @@ class Tag < ApplicationRecord
 		
 		# image = root_url + tagpic
 		return image
+	end
+
+
+	def self.dup
+		tagS = []
+		tags  = []
+		codes = []
+		regex = /[[:upper:]]/
+		Tag.all.each do |tag|
+		  small_tag_name = tag.name.downcase
+		  capital_tag_name = tag.name.titlecase
+
+		  # search
+		  small = Tag.where(name: "#{small_tag_name}")
+		  caps = Tag.where(name: "#{capital_tag_name}")
+		  
+		  if small.count >= 1 && caps.count >=1
+
+
+		  	# if small.count > caps.count
+
+		  	# elsif caps.count > small.count
+
+		  	# end
+		  			
+		  	# 
+
+		  	tagS << caps.first.vouchers.count
+		  	
+		  	tags << small.first.vouchers.count
+		  	
+		  end
+		end
+
+		return tags, tagS
 	end
 end
